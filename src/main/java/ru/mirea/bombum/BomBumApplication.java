78adbe0dbb7048a72515f82ac56be99f8376bba0
baklyNik <baklykov.nikita369@gmail.com>
2023-02-17 22:26:58 +0300

    Initial commit

---
+++ src/main/java/ru/mirea/bombum/BomBumApplication.java
@@ -0,0 +1,13 @@
+package ru.mirea.bombum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BomBumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BomBumApplication.class, args);
+    }
+
+}
